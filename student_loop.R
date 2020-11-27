8c98e83ce63548006d38c0c66e3c9b6d4caa34ca
moravecv <moravec.vojtech@gmail.com>
2020-11-27 15:17:39 +0100

    looping with excel

--- student_loop.R
+++ student_loop.R
@@ -1,9 +1,13 @@
 library(rmarkdown)
 library(pdftools)
+library(stringr)
+library(readxl)
 
+studenti <- read_excel("C:/Users/Vojta/Downloads/table_export.xlsx")
 
-
-jmena = c("anicka", "vojta", "karel")
+jmena <- studenti$Jméno
+jmena <- iconv(jmena, from = "UTF-8", to = "ASCII//TRANSLIT")
+jmena <- substr(x = jmena, start = 1, stop = 13)
 
 for(i in jmena){
   render("ZVH_TEST_COVID19_lin.Rmd",
@@ -17,7 +21,7 @@ testy_n <- list.files(path = "Testy_raw", pattern = ".pdf", full.names = F)
 
 
 for (i in 1:length(testy_n)){
-  pdf_subset(input = testy[i], pages = 1:3, output = paste0("Testy/Test_", testy_n[i]))
-  pdf_subset(input = testy[i], pages = 4, output = paste0("Vysledky/Vysledky_", testy_n[i]))
+  pdf_subset(input = testy[i], pages = 1:2, output = paste0("Testy/Test_", testy_n[i]))
+  pdf_subset(input = testy[i], pages = 3, output = paste0("Vysledky/Vysledky_", testy_n[i]))
 }
 
